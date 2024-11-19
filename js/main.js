989efb7c5691ff3191aa9e0bd68b8b97f4892e06
samrouth1234 <bichsamroth@gmail.com>
2024-11-19 23:21:58 +0700

    add space

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 function initializeTabs(tabListId, contentId) {
-  document.querySelectorAll(`#${tabListId} [role='tab']`).forEach(tab => {
-      tab.addEventListener('click', function () {
-          // Remove active class from all tabs
-          document.querySelectorAll(`#${tabListId} [role='tab']`).forEach(t => t.classList.remove('active-tab'));
-          
-          // Add active class to current tab
-          this.classList.add('active-tab');
-          
-          // Hide all tab panels
-          document.querySelectorAll(`#${contentId} [role='tabpanel']`).forEach(content => content.classList.add('hidden'));
-          
-          // Show target panel
-          const target = this.getAttribute('data-tabs-target');
-          document.querySelector(target).classList.remove('hidden');
-          
-          // Store active tab in local storage
-          localStorage.setItem(`${tabListId}-activeTab`, target);
-      });
-  });
+	document.querySelectorAll(`#${tabListId} [role='tab']`).forEach((tab) => {
+		tab.addEventListener('click', function () {
+			// Remove active class from all tabs
+			document.querySelectorAll(`#${tabListId} [role='tab']`).forEach((t) => t.classList.remove('active-tab'));
 
-  // Restore active tab on page load
-  window.addEventListener('DOMContentLoaded', function() {
-      const activeTab = localStorage.getItem(`${tabListId}-activeTab`);
-      if (activeTab) {
-          const activeButton = document.querySelector(`[data-tabs-target="${activeTab}"]`);
-          if (activeButton) {
-              activeButton.click();
-          }
-      }
-  });
+			// Add active class to current tab
+			this.classList.add('active-tab');
+
+			// Hide all tab panels
+			document.querySelectorAll(`#${contentId} [role='tabpanel']`).forEach((content) => content.classList.add('hidden'));
+
+			// Show target panel
+			const target = this.getAttribute('data-tabs-target');
+			document.querySelector(target).classList.remove('hidden');
+
+			// Store active tab in local storage
+			localStorage.setItem(`${tabListId}-activeTab`, target);
+		});
+	});
+
+	// Restore active tab on page load
+	window.addEventListener('DOMContentLoaded', function () {
+		const activeTab = localStorage.getItem(`${tabListId}-activeTab`);
+		if (activeTab) {
+			const activeButton = document.querySelector(`[data-tabs-target="${activeTab}"]`);
+			if (activeButton) {
+				activeButton.click();
+			}
+		}
+	});
 }
 
 // Initialize both sets of tabs
